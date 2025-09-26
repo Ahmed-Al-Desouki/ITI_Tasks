b1fd50799044f7c54619faab32faa30dde8bbb5f
Ahmed Al-Desouki <154838321+Ahmed-Al-Desouki@users.noreply.github.com>
2025-09-26 10:50:44 +0300

    Update Student.cs

--- Day3/EF-WinForms-/Models/Student.cs
+++ Day3/EF-WinForms-/Models/Student.cs
@@ -5,6 +5,7 @@ namespace WinForms_EF_CF.Models
         public int Id { get; set; }
         public string Name { get; set; } 
         public int Age { get; set; }
-        public string Department { get; set; } 
+        public string Dept { get; set; } 
     }
 }
+
