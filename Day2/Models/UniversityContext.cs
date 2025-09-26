0e1c03400fd30ba3580c8491c973b4564a7d5a1a
Ahmed Al-Desouki <154838321+Ahmed-Al-Desouki@users.noreply.github.com>
2025-09-26 10:39:52 +0300

    Day 2

---
+++ Day2/Models/UniversityContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+namespace WinFormsEfCrud.Models
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; } = null!;
+
+        public UniversityContext()
+        {
+        }
+
+        // edit this connection string to match your environment or use appsettings + DI
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=University;Trusted_Connection=True;Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.FirstName).HasMaxLength(100);
+                entity.Property(e => e.LastName).HasMaxLength(100);
+                entity.Property(e => e.Email).HasMaxLength(200);
+            });
+        }
+    }
+}
